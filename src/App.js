e6384e0f16e096f108422834a583427e1f8e4ade
fr-me-jim <fr.me.jim@gmail.com>
2019-07-28 23:19:30 +0200

    some start content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( <h1>Noticias API React</h1> );
+  }
 }
-
+ 
 export default App;
+
