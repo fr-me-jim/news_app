0da52e9ccd2be439e5f410a21a60c0df614ba7ea
fr-me-jim <fr.me.jim@gmail.com>
2019-07-28 23:40:15 +0200

    added initial api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {  }
+  state = { 
+    news: []
+   }
+  
+  componentDidMount(){
+    this.getNews();
+  }
+
+  getNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=323c894f6d7443dea2613c06482e22e3`
+
+    const response = await fetch(url);
+    const news = await response.json();
+
+    this.setState({
+      news: news.articles
+    })
+  }
+  
   render() { 
     return ( <h1>Noticias API React</h1> );
   }
